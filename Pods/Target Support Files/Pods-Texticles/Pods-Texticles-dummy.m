716df56ae64e03fa96b78ba8e86aff0f3d11e689
Eddie Char <emchar1@gmail.com>
2020-09-11 23:34:40 -0700

    Initial commit

---
+++ Pods/Target Support Files/Pods-Texticles/Pods-Texticles-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Texticles : NSObject
+@end
+@implementation PodsDummy_Pods_Texticles
+@end
---
+++ Texticles/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Texticles
+//
+//  Created by Eddie Char on 9/11/20.
+//  Copyright © 2020 Eddie Char. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
